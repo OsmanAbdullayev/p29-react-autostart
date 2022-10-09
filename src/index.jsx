1773da9eb95ec5ff52d3183d957d4641b414340e
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-09 22:38:40 +0400

    after repeating

---
+++ src/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./sass/style.css";
+import Nav from "./components/Nav";
+import Card from "./components/Card";
+
+
+class App extends Component {
+	render() {
+		return (
+			<>
+				<Nav />
+				<div className="container">
+        <div className="row gx-3">
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+        </div>
+			</>
+		);
+	}
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
+reportWebVitals();
